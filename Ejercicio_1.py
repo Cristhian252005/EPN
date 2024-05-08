3023e4170ead1f8d04bee20599fc6f27e3d79d53
Joe-575 <medinajoel814@gmail.com>
2024-05-08 04:11:18 +0000

    PRUEBA Prueba

---
+++ Ejercicio_1.py
@@ -0,0 +1,20 @@
+'''
+    Solicitar al usuario que ingrese su dirección email. Imprimir un mensaje indicando si la 
+    dirección es válida o no, valiéndose de una función para decidirlo. Una dirección se 
+    considerará válida si contiene el símbolo "@".
+'''
+
+
+
+def correo():
+    while True:
+        correo = input("\t\t Ingrese la dirección de su correo electrónico: ")
+        if '@' in correo:
+            print(" Su correo es válido")
+            break
+        else:
+            print(" Su correo no es válido")
+            True
+    
+       
+correo()
